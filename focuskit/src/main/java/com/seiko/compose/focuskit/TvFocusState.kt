9d0a6a3d39141580ff78d22ca823d6bc1d7944b8
Seiko <seiko_des@outlook.com>
2021-08-04 10:41:43 +0800

    TvFocusState extends FocusState

--- focuskit/src/main/java/com/seiko/compose/focuskit/TvFocusState.kt
+++ focuskit/src/main/java/com/seiko/compose/focuskit/TvFocusState.kt
@@ -1,11 +1,24 @@
 package com.seiko.compose.focuskit
 
-enum class TvFocusState {
-  None, Active, ActiveParent;
+import androidx.compose.ui.focus.FocusState
 
-  val isFocused: Boolean
+enum class TvFocusState : FocusState {
+  None,
+  Active,
+  ActiveParent;
+
+  override val isFocused: Boolean
     get() = when (this) {
       Active, ActiveParent -> true
       None -> false
     }
+
+  override val hasFocus: Boolean
+    get() = when (this) {
+      ActiveParent -> true
+      Active, None -> false
+    }
+
+  override val isCaptured: Boolean
+    get() = false
 }

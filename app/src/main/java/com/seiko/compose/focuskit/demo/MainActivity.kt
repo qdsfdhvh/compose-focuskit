b22c3e7294b5719e0c0d54711908681f146d0160
seiko <605590140@qq.com>
2021-08-29 20:16:55 +0800

    Add DemoApp

--- app/src/main/java/com/seiko/compose/focuskit/demo/MainActivity.kt
+++ app/src/main/java/com/seiko/compose/focuskit/demo/MainActivity.kt
@@ -40,21 +40,6 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    TvLogger.setLogger(object : TvLogger {
-      override var level: Int = Log.DEBUG
-      override fun log(level: Int, msg: String?, throwable: Throwable?) {
-        if (msg != null) {
-          Log.println(level, "Focuskit", msg)
-        }
-        if (throwable != null) {
-          val writer = StringWriter()
-          throwable.printStackTrace(PrintWriter(writer))
-          Log.println(level, "Focuskit", writer.toString())
-        }
-      }
-    })
-
     setContent {
       AnimeTvTheme {
         CompositionLocalProvider(

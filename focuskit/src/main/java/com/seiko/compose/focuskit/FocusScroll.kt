6cdaa7518d8bedb83d9d2732795be28e3b757d73
Seiko <seiko_des@outlook.com>
2021-09-09 14:34:33 +0800

    LazyListState make scroll in ScrollBehaviour

--- focuskit/src/main/java/com/seiko/compose/focuskit/FocusScroll.kt
+++ focuskit/src/main/java/com/seiko/compose/focuskit/FocusScroll.kt
@@ -1,33 +1,22 @@
 package com.seiko.compose.focuskit
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.gestures.animateScrollBy
-import androidx.compose.foundation.gestures.stopScroll
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Density
 import kotlinx.coroutines.launch
-
-private val Saver = mapSaver(
-  save = { mapOf("index" to it.index) },
-  restore = {  FocusIndexInteraction(it["index"] as? Int ?: 0) }
-)
-
-@Composable
-fun rememberFocusIndexInteraction(key: Any? = null): MutableState<FocusIndexInteraction> {
-  return rememberSaveable(key, stateSaver = Saver) {
-    mutableStateOf(FocusIndexInteraction(0))
-  }
-}
 
 @SuppressLint("UnnecessaryComposedModifier")
 fun Modifier.onFocusScroll(
@@ -47,7 +36,7 @@ fun Modifier.onFocusScroll(
     onDispose { }
   }
 
-  Modifier
+  this
     .onTvKeyEvent { key ->
       if (state.isScrollInProgress) {
         return@onTvKeyEvent true
@@ -63,22 +52,22 @@ fun Modifier.onFocusScroll(
         val interaction = FocusIndexInteraction(nextFocusIndex)
         interactionSource?.emit(interaction)
         focusIndexInteraction = interaction
-        state.scrollAndFocusTv(foundItem, scrollBehaviour, density)
+
+        scrollBehaviour.onScroll(state, foundItem, density)
       }
       true
     }
 }
 
-private suspend fun LazyListState.scrollAndFocusTv(
-  foundItem: LazyListItemInfo,
-  scrollBehaviour: ScrollBehaviour,
-  density: Density,
-) {
-  stopScroll()
+private val Saver = mapSaver(
+  save = { mapOf("index" to it.index) },
+  restore = { FocusIndexInteraction(it["index"] as? Int ?: 0) }
+)
 
-  val value = scrollBehaviour.calculateScrollBy(this, foundItem, density)
-  if (value != 0f) {
-    animateScrollBy(value, tween(SCROLL_ANIMATION_DURATION, 0, LinearEasing))
+@Composable
+fun rememberFocusIndexInteraction(key: Any? = null): MutableState<FocusIndexInteraction> {
+  return rememberSaveable(key, stateSaver = Saver) {
+    mutableStateOf(FocusIndexInteraction(0))
   }
 }
 
--- focuskit/src/main/java/com/seiko/compose/focuskit/ScrollBehaviour.kt
+++ focuskit/src/main/java/com/seiko/compose/focuskit/ScrollBehaviour.kt
@@ -1,60 +1,76 @@
 package com.seiko.compose.focuskit
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.stopScroll
 import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 
 interface ScrollBehaviour {
-  fun calculateScrollBy(state: LazyListState, focusItem: LazyListItemInfo, density: Density): Float
+  suspend fun onScroll(state: LazyListState, focusItem: LazyListItemInfo, density: Density)
 
   companion object {
-    val Horizontal: ScrollBehaviour = object : ScrollBehaviour {
-      override fun calculateScrollBy(
-        state: LazyListState,
-        focusItem: LazyListItemInfo,
-        density: Density,
-      ): Float {
-        val viewStart = state.layoutInfo.viewportStartOffset
-        val viewEnd = state.layoutInfo.viewportEndOffset
-        val viewSize = viewEnd - viewStart
-
-        val itemStart = focusItem.offset
-        val itemEnd = focusItem.offset + focusItem.size
-        val itemSize = focusItem.size
-
-        val leftLine = viewStart + viewSize * 0.3
-        val rightLine = viewStart + viewSize * 0.7
-
-        return when {
-          itemStart > rightLine -> itemSize.toFloat()
-          itemEnd < leftLine -> (-itemSize).toFloat()
-          else -> 0f
-        }
-      }
-    }
-
-    val Vertical: ScrollBehaviour = object : ScrollBehaviour {
-      override fun calculateScrollBy(
-        state: LazyListState,
-        focusItem: LazyListItemInfo,
-        density: Density
-      ): Float {
-        val viewStart = state.layoutInfo.viewportStartOffset
-        val viewEnd = state.layoutInfo.viewportEndOffset
-        val viewSize = viewEnd - viewStart
-
-        val itemStart = focusItem.offset
-        val itemEnd = focusItem.offset + focusItem.size
-
-        val offSect= density.run { 20.dp.roundToPx() }
-
-        return when {
-          itemStart < viewStart -> itemStart.toFloat() - offSect
-          itemEnd > viewStart + viewSize -> (itemEnd - viewSize - viewStart).toFloat() + offSect
-          else -> 0f
-        }
-      }
-    }
+    val Horizontal: ScrollBehaviour get() = HorizontalImpl
+    val Vertical: ScrollBehaviour get() = VerticalImpl
   }
 }
+
+private object HorizontalImpl : ScrollBehaviour {
+  override suspend fun onScroll(
+    state: LazyListState,
+    focusItem: LazyListItemInfo,
+    density: Density
+  ) {
+    val viewStart = state.layoutInfo.viewportStartOffset
+    val viewEnd = state.layoutInfo.viewportEndOffset
+    val viewSize = viewEnd - viewStart
+
+    val itemStart = focusItem.offset
+    val itemEnd = focusItem.offset + focusItem.size
+    val itemSize = focusItem.size
+
+    val leftLine = viewStart + viewSize * 0.3
+    val rightLine = viewStart + viewSize * 0.7
+
+    val value = when {
+      itemStart > rightLine -> itemSize.toFloat()
+      itemEnd < leftLine -> (-itemSize).toFloat()
+      else -> return
+    }
+    state.startScroll(value)
+  }
+}
+
+private object VerticalImpl : ScrollBehaviour {
+  override suspend fun onScroll(
+    state: LazyListState,
+    focusItem: LazyListItemInfo,
+    density: Density
+  ) {
+    val viewStart = state.layoutInfo.viewportStartOffset
+    val viewEnd = state.layoutInfo.viewportEndOffset
+    val viewSize = viewEnd - viewStart
+
+    val itemStart = focusItem.offset
+    val itemEnd = focusItem.offset + focusItem.size
+
+    val offSect = density.run { 20.dp.roundToPx() }
+
+    val value = when {
+      itemStart < viewStart -> itemStart.toFloat() - offSect
+      itemEnd > viewStart + viewSize -> (itemEnd - viewSize - viewStart).toFloat() + offSect
+      else -> return
+    }
+    state.startScroll(value)
+  }
+}
+
+private suspend fun LazyListState.startScroll(value: Float) {
+  stopScroll()
+  animateScrollBy(value, tween(SCROLL_ANIMATION_DURATION, 0, LinearEasing))
+}
+
+private const val SCROLL_ANIMATION_DURATION = 150

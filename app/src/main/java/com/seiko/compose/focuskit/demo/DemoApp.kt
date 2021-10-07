5f46575903bda3d6a9b65e1aa20f70b6f3d21a3c
seiko <605590140@qq.com>
2021-10-07 17:57:22 +0800

    Remove TvLogger

--- app/src/main/java/com/seiko/compose/focuskit/demo/DemoApp.kt
+++ app/src/main/java/com/seiko/compose/focuskit/demo/DemoApp.kt
@@ -1,28 +1,9 @@
 package com.seiko.compose.focuskit.demo
 
 import android.app.Application
-import android.util.Log
-import com.seiko.compose.focuskit.TvLogger
-import java.io.PrintWriter
-import java.io.StringWriter
 
 class DemoApp : Application() {
   override fun onCreate() {
     super.onCreate()
-    if (BuildConfig.DEBUG) {
-      TvLogger.setLogger(object : TvLogger {
-        override var level: Int = Log.DEBUG
-        override fun log(level: Int, msg: String?, throwable: Throwable?) {
-          if (msg != null) {
-            Log.println(level, "Focuskit", msg)
-          }
-          if (throwable != null) {
-            val writer = StringWriter()
-            throwable.printStackTrace(PrintWriter(writer))
-            Log.println(level, "Focuskit", writer.toString())
-          }
-        }
-      })
-    }
   }
 }

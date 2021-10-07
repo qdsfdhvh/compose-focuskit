95e665f3f370d7fe61e307119b436e31b26e48fe
seiko <605590140@qq.com>
2021-10-07 13:49:55 +0800

    update jitpack version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
   implementation("androidx.activity:activity-compose:1.3.1")
-  implementation("androidx.navigation:navigation-compose:2.4.0-alpha06")
-  implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02")
+  implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
+  implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01")
   implementation("io.coil-kt:coil-compose:1.3.2")
 
   implementation(project(":focuskit"))

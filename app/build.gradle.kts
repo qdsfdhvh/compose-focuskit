d0fde8fb34f0a4d9fce97b5b1624306c9b0c9d93
seiko <605590140@qq.com>
2021-10-12 22:49:03 +0800

    coil to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
   implementation("androidx.activity:activity-compose:1.3.1")
   implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
-  implementation("io.coil-kt:coil-compose:1.3.2")
+  implementation("io.coil-kt:coil-compose:1.4.0")
 
   implementation(project(":focuskit"))
   implementation(project(":focuskit-player"))
